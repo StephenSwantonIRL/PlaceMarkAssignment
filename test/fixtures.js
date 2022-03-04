a68587a5e50fca2bb80f01bc2d8bfe723338d565
Stephen <stephenswanton@gmail.com>
2022-03-04 17:56:50 +0000

    test: updated place mark tests for create delete and update

--- test/fixtures.js
+++ test/fixtures.js
@@ -53,6 +53,39 @@ export const longplayer = {
   latitude: 51.508514,
   longitude: 0.008079,
   description: "<p>If you miss hearing Longplayer on your next trip to London, you’ll get the chance to catch it again—the musical composition will be playing in the old lighthouse at Trinity Buoy Wharf for the next 1,000 years. Longplayer consists of six short recorded pieces written for Tibetan singing bowls that are transposed and combined in such a way that the variations will never repeat during the song’s millennium-long run. It began playing on December 31, 1999, and is scheduled to end in the dying seconds of 2999.</p><p>Custodians of the project have established the Longplayer Trust to devise ways of keeping the music alive in the face of the inevitable technological and social changes that will occur over the next ten centuries.</p>",
-  images: []
+  images: [],
+  createdBy: "1223355634"
 };
 
+export const svalbard = {
+  name: "Svalbard Global Seed Vault",
+  location: "Longyearbyen, Svalbard, Norway",
+  latitude: 78.238166,
+  longitude: 15.447236,
+  description: "<p>A winter night in Longyearbyen lasts 4 months. In the ice-covered mountains, the darkness is broken only by a slim concrete building that emits a pale blue glow as it overlooks the 1,000-resident town. The simple structure offers no hint as to what’s protected inside: a collection of seeds that could save humanity.</p><p>Due to the loss of genetic diversity among commercially cultivated crops, which tend to be grown from clonal monocultures, many worldwide food crops are at risk of disease. Mutated strains of fungus, or a new bacterium, could potentially wipe out an entire world crop in a matter of months, causing massive food shortages. The Svalbard Global Seed Vault was established by the Norwegian government in 2008 to function as a sort of genetic safe-deposit box.</p><p>The facility has the capacity to conserve 4.5 million seed samples. Under the current temperature conditions in the vault, which are similar to those in a kitchen freezer, the seed samples can remain viable to begin new crops for anywhere from 2,000 to 20,000 years.</p><p>Svalbard was chosen as the location because it is tectonically stable and its permafrost provides natural refrigeration in case of a power failure. There is no permanent staff at the seed bank, but it is monitored constantly using electronic surveillance. Access to the vaults, open only to employees, requires passing four locked doors protected by coded access keys.</p>",
+  images: [],
+  createdBy: "1223355634" 
+
+}
+
+
+export const incompleteSvalbard = {
+  name: "Svalbard Global Seed Vault",
+  location: "Longyearbyen, Svalbard, Norway",
+  latitude: 78.238166,
+  description: "<p>A winter night in Longyearbyen lasts 4 months. In the ice-covered mountains, the darkness is broken only by a slim concrete building that emits a pale blue glow as it overlooks the 1,000-resident town. The simple structure offers no hint as to what’s protected inside: a collection of seeds that could save humanity.</p><p>Due to the loss of genetic diversity among commercially cultivated crops, which tend to be grown from clonal monocultures, many worldwide food crops are at risk of disease. Mutated strains of fungus, or a new bacterium, could potentially wipe out an entire world crop in a matter of months, causing massive food shortages. The Svalbard Global Seed Vault was established by the Norwegian government in 2008 to function as a sort of genetic safe-deposit box.</p><p>The facility has the capacity to conserve 4.5 million seed samples. Under the current temperature conditions in the vault, which are similar to those in a kitchen freezer, the seed samples can remain viable to begin new crops for anywhere from 2,000 to 20,000 years.</p><p>Svalbard was chosen as the location because it is tectonically stable and its permafrost provides natural refrigeration in case of a power failure. There is no permanent staff at the seed bank, but it is monitored constantly using electronic surveillance. Access to the vaults, open only to employees, requires passing four locked doors protected by coded access keys.</p>",
+  images: [],
+  createdBy: "1223355634"
+
+}
+
+export const updatedSvalbard = {
+  name: "Svalbard Global Seed Vault",
+  location: "Longyearbyen, Svalbard, Norway",
+  latitude: 78.238166,
+  longitude: 15.447236,
+  description: "<p>A winter night in Longyearbyen lasts 5 months. In the ice-covered mountains, the darkness is broken only by a slim concrete building that emits a pale blue glow as it overlooks the 1,000-resident town. The simple structure offers no hint as to what’s protected inside: a collection of seeds that could save humanity.</p><p>Due to the loss of genetic diversity among commercially cultivated crops, which tend to be grown from clonal monocultures, many worldwide food crops are at risk of disease. Mutated strains of fungus, or a new bacterium, could potentially wipe out an entire world crop in a matter of months, causing massive food shortages. The Svalbard Global Seed Vault was established by the Norwegian government in 2008 to function as a sort of genetic safe-deposit box.</p><p>The facility has the capacity to conserve 4.5 million seed samples. Under the current temperature conditions in the vault, which are similar to those in a kitchen freezer, the seed samples can remain viable to begin new crops for anywhere from 2,000 to 20,000 years.</p><p>Svalbard was chosen as the location because it is tectonically stable and its permafrost provides natural refrigeration in case of a power failure. There is no permanent staff at the seed bank, but it is monitored constantly using electronic surveillance. Access to the vaults, open only to employees, requires passing four locked doors protected by coded access keys.</p>",
+  images: [],
+  createdBy: "1223355634"
+
+}
--- test/places-model-test.js
+++ test/places-model-test.js
@@ -1,6 +1,6 @@
 import { assert } from "chai";
 import { db } from "../src/models/db.js";
-import { longplayer, maggie, testUsers } from "./fixtures.js";
+import { longplayer, svalbard, incompleteSvalbard, updatedSvalbard } from "./fixtures.js";
 import { assertSubset } from "./test-utils.js";
 
 suite("Place Model tests", () => {
@@ -15,14 +15,53 @@ suite("Place Model tests", () => {
     assert.isFalse(assertSubset(longplayer, allPlacesPre));
     const newPlace = await db.placeStore.addPlace(longplayer);
     const allPlacesPost = await db.placeStore.getAllPlaces();
-    console.log(newPlace);
     assertSubset(longplayer, allPlacesPost);
   });
 
-  test("delete a place - fail", async () => {
+  test("create a place - failed - missing required parameter ", async () => {
+    await db.placeStore.addPlace(incompleteSvalbard);
+    const allPlaces = await db.placeStore.getAllPlaces();
+    assert.isFalse(assertSubset(incompleteSvalbard, allPlaces));
+  });
+
+  test("delete a place - fail - bad id ", async () => {
     const allPlacesPre = await db.placeStore.getAllPlaces();
-    await db.placeStore.deletePlaceById("bad-id");
+    await db.placeStore.deletePlaceById("bad-id", "user");
     const allPlacesPost = await db.placeStore.getAllPlaces();
     assert.equal(allPlacesPre.length, allPlacesPost.length);
   });
+
+  test("delete a place - fail - not created by current user", async () => {
+    const placeToDelete = await db.placeStore.addPlace(svalbard);
+    const placeId = placeToDelete._id;
+    const allPlacesPre = await db.placeStore.getAllPlaces();
+    const currentUser = "125634"
+    await db.placeStore.deletePlaceById(placeId, currentUser);
+    const allPlacesPost = await db.placeStore.getAllPlaces();
+    assert.equal(allPlacesPre.length, allPlacesPost.length);
+    assertSubset( placeToDelete, allPlacesPost);
+  });
+
+  test("delete a place - success ", async () => {
+    const placeToDelete = await db.placeStore.addPlace(longplayer);
+    const allPlacesPre = await db.placeStore.getAllPlaces();
+    const currentUserId = placeToDelete.createdBy;
+    await db.placeStore.deletePlaceById(placeToDelete._id, currentUserId);
+    const allPlacesPost = await db.placeStore.getAllPlaces();
+    assert.equal(allPlacesPre.length, allPlacesPost.length);
+    assert.isFalse(assertSubset(longplayer, allPlacesPost));
+  });
+
+  test("update a place - success", async () => {
+    const place = await db.placeStore.addPlace(svalbard);
+    svalbard._id = place._id;
+    const editor = place.createdBy
+    assert.deepEqual(place, svalbard);
+    const updatedPlace = updatedSvalbard;
+    updatedPlace._id = place._id;
+    await db.placeStore.updatePlace(place._id, updatedPlace);
+    const finalPlace = await db.placeStore.getPlaceById(place._id);
+    assert.deepEqual(finalPlace, updatedPlace);
+  });
+
 });
