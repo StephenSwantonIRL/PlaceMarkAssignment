bf4a991f02136ac572d9a4dc4401a8a31e8b69f4
Stephen <stephenswanton@gmail.com>
2022-03-06 16:55:37 +0000

    test: user api tests

---
+++ test/api/user-api-test.js
@@ -0,0 +1,55 @@
+import _ from "lodash";
+import { assert } from "chai";
+import { assertSubset } from "../test-utils.js";
+import { placeMarkService } from "./placemark-service.js";
+import { maggie, testUsers } from "../fixtures.js";
+import { db } from "../../src/models/db.js";
+
+
+suite("User API tests", () => {
+  setup(async () => {
+    await placeMarkService.deleteAllUsers();
+    for (let i = 0; i < testUsers.length; i += 1) {
+      // eslint-disable-next-line no-await-in-loop
+      testUsers[i] = await placeMarkService.createUser(testUsers[i]);
+    }
+  });
+
+  test("create a user", async () => {
+    const newUser = await placeMarkService.createUser(maggie);
+    assertSubset(maggie, newUser);
+    assert.isDefined(newUser._id);
+  });
+
+  // test("delete all userApi", async () => {
+  //   let returnedUsers = await placeMarkService.getAllUsers();
+  //   assert.equal(returnedUsers.length, 3);
+  //   await placeMarkService.deleteAllUsers();
+  //   returnedUsers = await placeMarkService.getAllUsers();
+  //   assert.equal(returnedUsers.length, 0);
+  // });
+
+  test("get a user", async () => {
+    const returnedUser = await placeMarkService.getUser(testUsers[0]._id);
+    assert.deepEqual(testUsers[0], returnedUser);
+  });
+
+  test("get a user - bad id", async () => {
+    try {
+      const returnedUser = await placeMarkService.getUser("1234");
+      assert.fail("Should not return a response");
+    } catch (error) {
+      assert(error.response.data.message === "No User with this id");
+    }
+  });
+
+  test("get a user - deleted user", async () => {
+    await placeMarkService.deleteAllUsers();
+    try {
+      const returnedUser = await placeMarkService.getUser(testUsers[0]._id);
+      assert.fail("Should not return a response");
+    } catch (error) {
+      assert(error.response.data.message === "No User with this id");
+    }
+  });
+});
